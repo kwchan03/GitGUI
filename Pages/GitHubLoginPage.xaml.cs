1bd0ff78ae2ddb5966cafca52d381249050c9e8d
Chan Kai Wen <chankaiwen2003@gmail.com>
2025-06-02 13:20:01 +0800

    done some frontend

---
+++ Pages/GitHubLoginPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GitGUI.Pages
+{
+    /// <summary>
+    /// Interaction logic for GitHubLogin.xaml
+    /// </summary>
+    public partial class GitHubLoginPage : Page
+    {
+        
+        public GitHubLoginPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
--- Pages/OperationPage.xaml.cs
+++ Pages/OperationPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,17 +13,21 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GitGUI
+namespace GitGUI.Pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Operation.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class OperationPage : Page
     {
-        public MainWindow()
+        public OperationPage()
         {
             InitializeComponent();
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
